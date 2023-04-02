91fe25edec16f79642fcca4254eef3a3f935f9eb
remixalpha <swapnarahul170@gmail.com>
2023-04-02 15:12:04 +0530

    HomePage inserted

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import UserHomePage from './components/UserProfilePage';
 import HRHomePage from './components/HRProfilePage';
 import HRProfilePage from './components/HRProfilePage';
 
+import { Navigate } from 'react-router-dom';
+
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "./assets/theme";
+import Presentation from "./layouts/pages/presentation";
+
 function App() {
   
   return (
@@ -14,7 +25,10 @@ function App() {
 
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" component={LoginRegistrationPage} />
+        <Route path="/presentation" element={<Presentation />} />
+        <Route path="*" element={<Navigate to="/presentation" />} />
+        
+        <Route exact path="/loginregisteration" component={LoginRegistrationPage} />
         <Route path="/user-home" component={UserHomePage} />
         <Route path="/hr-home" component={HRHomePage} />
         <Route path="/hr-profile" component={HRProfilePage} />
