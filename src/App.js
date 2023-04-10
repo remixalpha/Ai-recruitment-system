62c5bf73023a4f2672d516c333cd5ab588f17f70
kpndevroot <vishnukpn884@gmail.com>
2023-04-10 23:33:16 +0530

    tawind fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
- import Home from "./pages/Home"
+import Home from "./pages/Home";
 import LoginRegistrationPage from "./components/LoginRegistrationPage";
- import UserHomePage from "./components/UserProfilePage";
- import HRHomePage from "./components/HRProfilePage";
- import HRProfilePage from "./components/HRProfilePage";
+import UserHomePage from "./components/UserProfilePage";
+import HRHomePage from "./components/HRProfilePage";
+import HRProfilePage from "./components/HRProfilePage";
 
-
-import './css/style.css';
-
-
+import "./css/style.css";
 
 function App() {
-
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          exact
-          path="/"
-          element={<Home />} />
-        <Route path="/loginregisterpage" element={<LoginRegistrationPage />}/>
-        <Route path="/user-home" element={<UserHomePage />} /> 
-         <Route path="/hr-home" component={HRHomePage} /> 
-         <Route path="/hr-profile" component={HRProfilePage} /> 
+        <Route exact path="/" element={<Home />} />
+        <Route path="/loginregisterpage" element={<LoginRegistrationPage />} />
+        <Route path="/user-home" element={<UserHomePage />} />
+        <Route path="/hr-home" component={HRHomePage} />
+        <Route path="/hr-profile" component={HRProfilePage} />
       </Routes>
     </BrowserRouter>
   );
